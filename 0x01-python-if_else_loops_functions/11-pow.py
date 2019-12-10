e8aab994061f74348fdfec8679f7eaae132fc6bd
jzamora5 <jzamora_5@yahoo.com>
2019-12-10 18:22:33 +0000

    Change to Simple Method

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    res = a
-    if b == 0:
-        return (1)
-    elif b < 0:
-        res = 1
-        for i in range(b * -1):
-            res /= a
-    else:
-        for i in range(b - 1):
-            res *= a
-    return (res)
+    return (a ** b)
