2a1f39ebc528733c2053b343db20faab76a0bd45
jzamora5 <jzamora_5@yahoo.com>
2020-01-08 13:32:15 +0000

    Print List Safe

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    if not my_list or x == 0:
+        return (0)
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+    except:
+        i -= 1
+    finally:
+        print()
+        return(i + 1)
