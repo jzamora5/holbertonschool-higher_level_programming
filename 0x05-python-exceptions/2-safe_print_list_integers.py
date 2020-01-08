60e425212aa67fed58d6994ca524671441708bac
jzamora5 <jzamora_5@yahoo.com>
2020-01-08 14:00:23 +0000

    Safe Print List with ints

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    if not my_list or x == 0:
+        return (0)
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            pass
+        else:
+            count += 1
+    print()
+    return (count)
