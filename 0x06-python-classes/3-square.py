ee4578b15f74f8caa452446bc57d7799ab54f288
jzamora5 <jzamora_5@yahoo.com>
2020-01-09 15:47:20 +0000

    Add Area of square method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+
+    """ Class Square that defines methods and attributes for a square object"""
+
+    def __init__(self, size=0):
+        """ Class Constructor """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Method that calculates current square area """
+        return (self.__size * self.__size)
