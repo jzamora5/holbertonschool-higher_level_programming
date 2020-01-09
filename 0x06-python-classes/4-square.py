9a78ebdd9d873cae21278a200b3127d7c75c9031
jzamora5 <jzamora_5@yahoo.com>
2020-01-09 15:28:25 +0000

    Add Getter-Setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+
+    """ Class Square that defines methods and attributes for a square object"""
+
+    def __init__(self, size=0):
+            self.size = size
+
+    @property
+    def size(self):
+        """ Private Attribute size Getter """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ Private Attribute size Setter """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Method that calculates current square area """
+        return (self.__size * self.__size)
