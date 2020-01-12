e309ddfea8654ff0deaf5da29ed28608a54503b8
jzamora5 <jzamora_5@yahoo.com>
2020-01-12 04:30:52 +0000

    dot matrix multiplication NumPy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for multiplying two matrices with numpy
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function that multiplies 2 matrices
+
+    Args:
+
+    m_a: matrix a
+    m_b: matrix b
+
+    -m_a and m_b must be a list of lists of ints or floats
+    Else an error is raised
+    """
+
+    return (np.dot(m_a, m_b))
