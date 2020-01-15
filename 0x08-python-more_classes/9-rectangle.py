11b31405f42bab9b9322e63e69058aff982c5f47
jzamora5 <jzamora_5@yahoo.com>
2020-01-15 11:44:29 +0000

    Fix assigment order

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Constructor """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
