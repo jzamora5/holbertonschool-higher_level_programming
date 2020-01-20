fdb08838162c707bc5b25f311d22f86e3c9afe8f
jzamora5 <jzamora_5@yahoo.com>
2020-01-20 17:14:05 +0000

    Add Not Implemented Area Function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+class BaseGeometry:
+    """ Geometry Class """
+    def area(self):
+        """ Not Yet Implemented """
+        raise Exception("area() is not implemented")
