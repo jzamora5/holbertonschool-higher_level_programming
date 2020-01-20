65c346ae52a6c934dc15b67465d9ada0e6915875
jzamora5 <jzamora_5@yahoo.com>
2020-01-20 19:47:34 +0000

    Super Method Calls

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Class that inherits from Rectangle """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class that defines a Square by inheritance of Rectangle class """
+
+    def __init__(self, size):
+        """ Constructor """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+
+    def area(self):
+        """ Area method through Super call """
+        return super().area()
