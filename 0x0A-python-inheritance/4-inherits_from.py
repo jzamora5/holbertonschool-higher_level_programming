55aadb1a4a01911638374b6dd64621d1019f7bff
jzamora5 <jzamora_5@yahoo.com>
2020-01-20 16:51:58 +0000

    Add function to check if only inherits

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class ;
+    otherwise False """
+    return False if type(obj) == a_class else isinstance(obj, a_class)
