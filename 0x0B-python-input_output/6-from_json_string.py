abccf5d6895a46f9e7f0eeda1f8d43d0e4d9c017
jzamora5 <jzamora_5@yahoo.com>
2020-01-21 14:42:35 +0000

    ADD Func json to object (string)

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" json to string object function """
+
+import json
+
+
+def from_json_string(my_str):
+    """ function that returns an object (Python data structure) represented
+    by a JSON string"""
+    return json.loads(my_str)
