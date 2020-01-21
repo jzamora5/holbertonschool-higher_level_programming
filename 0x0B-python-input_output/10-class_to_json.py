634dd786f2e9feeeb47815fd851790b1e08da3f3
jzamora5 <jzamora_5@yahoo.com>
2020-01-21 15:37:54 +0000

    Func to Return __dict__ of a class

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object:
+    """
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__.copy()
