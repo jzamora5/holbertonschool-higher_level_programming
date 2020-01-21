6e6fd136d3e17307fa0c801b84af1523dd9cfac7
jzamora5 <jzamora_5@yahoo.com>
2020-01-21 13:43:30 +0000

    ADD Read N number of lines from file func

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def read_lines(filename="", nb_lines=0):
+    """ function that reads n lines of a text file (UTF8) and prints it
+    to stdout"""
+
+    with open("my_file_0.txt") as myfile:
+        lines = myfile.readlines()
+        if nb_lines <= 0 or nb_lines >= len(lines):
+            [print(i, end='') for i in lines]
+        else:
+            [print(lines[i], end='') for i in range(0, nb_lines)]
