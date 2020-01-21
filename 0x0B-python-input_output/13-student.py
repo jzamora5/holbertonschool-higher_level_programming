af8b7dda3563220ecafcab42e6a06bd78dd15fd5
jzamora5 <jzamora_5@yahoo.com>
2020-01-21 16:27:10 +0000

    ADD Method to replace from json

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+""" Student to json """
+
+
+class Student:
+    """ Class that defines a Student object """
+
+    def __init__(self, first_name, last_name, age):
+        """ Class Constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves Class dictionary base on attr
+        If attr is a list of strings, only attributes contained in that list
+        will be retrieved
+
+        Otherwise all  attributes will be retrieved
+        """
+        if not isinstance(attrs, list) or \
+           not all(isinstance(i, str) for i in attrs):
+
+            dic = self.__dict__.copy()
+        else:
+
+            dic = {i: self.__dict__[i] for i in attrs if i in self.__dict__}
+
+        return dic
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance """
+        for i in json:
+            self.__dict__[i] = json[i]
