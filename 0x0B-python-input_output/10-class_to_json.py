2d863b65ea2b9382feafd087467400828c6244a2
jzamora5 <jzamora_5@yahoo.com>
2020-01-21 15:41:13 +0000

    Fix case for when no __dict__

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -6,5 +6,7 @@ def class_to_json(obj):
     structure (list, dictionary, string, integer and boolean) for JSON
     serialization of an object:
     """
+    dic = {}
     if hasattr(obj, "__dict__"):
-        return obj.__dict__.copy()
+        dic = obj.__dict__.copy()
+    return dic
