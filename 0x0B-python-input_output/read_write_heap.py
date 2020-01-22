47583d3d93ab8e91ee0c630b3bd3c5c7025646bb
jzamora5 <jzamora_5@yahoo.com>
2020-01-22 04:42:00 +0000

    Fix Shebang

--- 0x0B-python-input_output/read_write_heap.py
+++ 0x0B-python-input_output/read_write_heap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """script that finds a string in the heap of a running process, and replaces it
 
 Usage: sudo ./read_write_heap.py pid search_string replace_string"""
