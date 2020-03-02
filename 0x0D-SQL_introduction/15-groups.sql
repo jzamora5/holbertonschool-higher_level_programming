086dd4afae257a4d411c64b012bd9c0cba22af36
jzamora5 <jzamora_5@yahoo.com>
2020-03-02 19:41:24 +0000

    GROUP BY

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+--  Lists the number of records with the same score in the table second_table
+-- Query to perform operation
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
