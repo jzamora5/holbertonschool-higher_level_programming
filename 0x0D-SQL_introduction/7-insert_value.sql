9c2a0c6621ce6f57e5871be83557c0ccaa2af321
jzamora5 <jzamora_5@yahoo.com>
2020-03-02 17:21:58 +0000

    ADD Backticks

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in the first_table in the database used
 -- Query to perform operation
-INSERT INTO `first_table` (id,name) VALUES (11, "Holberton School")
+INSERT INTO `first_table` (`id`, `name`) VALUES (11, "Holberton School")
