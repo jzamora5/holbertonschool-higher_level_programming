d62ecd40e38bfdf549970814d37a6f34e1581ad8
jzamora5 <jzamora_5@yahoo.com>
2020-03-03 19:56:26 +0000

    JOIN WITH COUNT

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- Query to perform operation
-SELECT gr.name, COUNT(shgr.genre_id) AS nm FROM tv_genres AS gr
+SELECT gr.name, COUNT(shgr.genre_id) AS number_of_shows FROM tv_genres AS gr
        JOIN tv_show_genres AS shgr
        ON gr.id=shgr.genre_id
        GROUP BY shgr.genre_id
-       ORDER BY nm DESC;
+       ORDER BY number_of_shows DESC;
