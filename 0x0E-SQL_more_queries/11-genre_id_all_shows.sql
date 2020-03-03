81fa1f001a024a3ab48e20777223049a0b30932c
jzamora5 <jzamora_5@yahoo.com>
2020-03-03 19:31:12 +0000

    LEFT JOIN

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
--- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Lists all shows contained in the database hbtn_0d_tvshows.
 -- Query to perform operation
 SELECT sh.title, shgr.genre_id FROM tv_shows AS sh
        LEFT JOIN tv_show_genres AS shgr ON sh.id=shgr.show_id
