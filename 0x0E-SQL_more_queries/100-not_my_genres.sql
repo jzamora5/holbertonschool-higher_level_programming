d4e4bf8c111029bda532269b11dfb847161cc596
jzamora5 <jzamora_5@yahoo.com>
2020-03-03 22:14:12 +0000

    JOIN with subquery

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,16 @@
+-- List all genres not linked to the show Dexter
+-- Query to perform operation
+SELECT gr.name FROM tv_genres AS gr
+LEFT JOIN
+(
+       SELECT gr.id, gr.name FROM tv_genres AS gr
+       JOIN tv_show_genres AS shgr
+       	    ON gr.id=shgr.genre_id
+       JOIN tv_shows AS sh
+       	    ON shgr.show_id=sh.id
+       WHERE sh.title = "Dexter"
+       ORDER BY gr.name ASC
+) AS subdx
+ON subdx.id = gr.id
+WHERE subdx.name IS NULL
+ORDER BY subdx.name;
