9c404b57e5108706e04cdac615dfe9c5dceb3304
jzamora5 <jzamora_5@yahoo.com>
2020-03-03 19:24:32 +0000

    Join IDs

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Query to perform operation
+SELECT sh.title, shgr.genre_id FROM tv_shows AS sh
+       JOIN tv_show_genres AS shgr ON sh.id=shgr.show_id
+       ORDER BY sh.title, shgr.genre_id ASC;
