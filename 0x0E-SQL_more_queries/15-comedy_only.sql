20e064f1d178c5177ff4b109059a2b94205ba612
jzamora5 <jzamora_5@yahoo.com>
2020-03-03 20:56:36 +0000

    Double Join in another direction

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all Comedy shows in the database hbtn_0d_tvshows.
+-- Query to perform operation
+SELECT sh.title name FROM tv_shows AS sh
+       JOIN tv_show_genres AS shgr
+       ON sh.id=shgr.show_id
+       JOIN tv_genres AS gr
+       ON shgr.genre_id=gr.id
+       WHERE gr.name = "Comedy"
+       ORDER BY sh.title ASC;
