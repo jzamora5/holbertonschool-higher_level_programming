4ed32dce708d340e985d13cfda567cb0652b538b
jzamora5 <jzamora_5@yahoo.com>
2020-03-03 17:10:49 +0000

    Add Order BY although default

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa
 -- Query to perform operation
 SELECT c.id, c.name FROM cities AS c, states AS s
-       WHERE c.state_id=s.id AND s.name = "California";
+       WHERE c.state_id=s.id AND s.name = "California"
+       ORDER BY c.id ASC;
