b03eb24e2217d7aba11fc66427de21cad61bb2e0
jzamora5 <jzamora_5@yahoo.com>
2020-03-28 18:14:53 +0000

    Create table in file

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
