ac359e600e6a7322dda91f434f47f178a9b5f2ca
jzamora5 <jzamora_5@yahoo.com>
2020-04-14 15:41:45 +0000

    Json API Corrections

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,15 +5,16 @@ if __name__ == "__main__":
     import requests
     import sys
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) > 1):
-        q = sys.argv[1]
+    if len(sys.argv) > 1:
+        data = {'q': sys.argv[1]}
     else:
-        q = ""
-
+        data = {'q': ""}
     try:
-        r = requests.get(url)
+        r = requests.post(url, data)
         js = r.json()
-        print("Good")
-        print(js)
+        if not js:
+            print("No result")
+        else:
+            print("[{}] {}".format(js['id'], js['name']))
     except ValueError:
-        print("Error")
+        print("Not a valid JSON")
