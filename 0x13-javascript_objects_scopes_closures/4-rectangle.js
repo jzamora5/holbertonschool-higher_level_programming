a5d58ebfd93ee528ff0f26a4b9e3a9a657513840
jzamora5 <jzamora_5@yahoo.com>
2020-05-11 14:24:14 +0000

    JavaScript Rectangle Class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const character = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
