9fc059811d52ee76ee53af5a97ac583d89200019
jzamora5 <jzamora_5@yahoo.com>
2020-05-12 14:53:44 +0000

    Fix UTF8 and Basic Scrap

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@ const argv = process.argv;
 const fs = require('fs');
 const fileName = argv[2];
 const string = argv[3];
-fs.writeFile(fileName, string, function (err) {
+fs.writeFile(fileName, string, 'utf-8', function (err) {
   if (err) return console.log(err);
 });
