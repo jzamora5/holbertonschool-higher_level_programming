1242daa8be9aa4792eb36dbecf4b95325a9748de
jzamora5 <jzamora_5@yahoo.com>
2020-05-12 17:36:52 +0000

    Try another method for string

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
 const argv = process.argv;
 const url = argv[2];
-const urlChar = 'https://swapi-api.hbtn.io/api/people/18/';
+const id = '18';
 const request = require('request');
 request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    try {
-      const rbody = JSON.parse(body);
-      let count = 0;
-      for (const i of rbody.results) {
-        if (i.characters.includes(urlChar)) {
+    const rbody = JSON.parse(body);
+    let count = 0;
+    for (const i of rbody.results) {
+      for (const j of i.characters) {
+        if (j.search(id) > 0) {
           count++;
         }
       }
-      console.log(count);
-    } catch (err) {
-      console.error('error:', err);
     }
+    console.log(count);
   }
 });
