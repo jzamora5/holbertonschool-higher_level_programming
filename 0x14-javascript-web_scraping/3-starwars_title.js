3ada2add5d5833f7a0ab6bfe32730d95650667f1
jzamora5 <jzamora_5@yahoo.com>
2020-05-12 17:16:14 +0000

    Remove String Literal

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const id = argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2] + '/';
 const request = require('request');
-request(`${url}${id}/`, function (error, response, body) {
+
+request(url, function (error, response, body) {
   if (error === null) {
     console.log(JSON.parse(body).title);
   }
