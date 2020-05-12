0f20df37b38082bf483392b1df549f0c41b241c7
jzamora5 <jzamora_5@yahoo.com>
2020-05-12 17:08:01 +0000

    Try without Error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,14 +3,8 @@ const argv = process.argv;
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const id = argv[2];
 const request = require('request');
-request(`${url}${id}`, function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
-  } else {
-    try {
-      console.log(JSON.parse(body).title);
-    } catch (err) {
-      console.error('error:', err);
-    }
+request(`${url}${id}/`, function (error, response, body) {
+  if (!error) {
+    console.log(JSON.parse(body).title);
   }
 });
