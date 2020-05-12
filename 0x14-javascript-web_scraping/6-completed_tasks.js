47147c92754f258a4874689bccdf70904e3b0b1d
jzamora5 <jzamora_5@yahoo.com>
2020-05-12 17:42:20 +0000

    Remove Json Try

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,20 +6,16 @@ request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    try {
-      const rbody = JSON.parse(body);
-      const dict = {};
-      for (const i of rbody) {
-        if (!dict[i.userId]) {
-          dict[i.userId] = 0;
-        }
-        if (i.completed) {
-          dict[i.userId] += 1;
-        }
+    const rbody = JSON.parse(body);
+    const dict = {};
+    for (const i of rbody) {
+      if (!dict[i.userId]) {
+        dict[i.userId] = 0;
       }
-      console.log(dict);
-    } catch (err) {
-      console.error('error:', err);
+      if (i.completed) {
+        dict[i.userId] += 1;
+      }
     }
+    console.log(dict);
   }
 });
