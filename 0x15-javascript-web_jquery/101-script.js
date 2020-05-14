75e730235ec9834f699942b43b811f7535f24690
jzamora5 <jzamora_5@yahoo.com>
2020-05-14 16:41:44 +0000

    ADD REMOVE CLEAR List

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,16 +1,12 @@
-$(document).ready(() => {
+$(document).ready(function () {
   $('DIV#add_item').click(() => {
     $('UL.my_list').append('<li>Item</li>');
   });
-});
 
-$(document).ready(() => {
   $('DIV#remove_item').click(() => {
     $('UL.my_list>li:last-child').remove();
   });
-});
 
-$(document).ready(() => {
   $('DIV#clear_list').click(() => {
     $('UL.my_list>li').remove();
   });
